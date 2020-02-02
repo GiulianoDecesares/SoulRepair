47c31f75fc6d57acadb54d642fcb75713fb07f77
Giuliano Decesares <giulianodecesares@gmail.com>
2020-02-02 13:01:40 -0300

    Added player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,8 +18,10 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private float gravity;
 	[SerializeField] private float rotationSpeed;
 
+	[Header("Animations")] 
+	[SerializeField] private TheDeathAnimator animator;
+	
 	private CharacterController characterController;
-	Vector3 direction = Vector3.zero;
 
 	private List<WeakReference<IEnemy>> spottedEnemies;
 
@@ -38,49 +40,19 @@ public class PlayerController : MonoBehaviour
 		
 		this.characterController.Move(move * this.movementSpeed);
 		this.transform.Rotate(rotation);
-		
+
+		// Manage animations
+		this.animator.Walk(rotation.magnitude + move.magnitude);
+
 		if (Input.GetKeyDown(this.attack))
+		{
+			this.animator.Slash();
 			this.Attack();
-		
-		/*
-		if (this.characterController != null)
-		{
-			float horizontalAxis = Input.GetAxis("Horizontal");
-			float verticalAxis = Input.GetAxis("Vertical");
-			float rotateDegreesPerSecond = 180f;
-
-			if (this.characterController.isGrounded)
-			{
-				direction = new Vector3(horizontalAxis, 0.0f, verticalAxis);
-				direction = this.transform.TransformDirection(direction);
-			}
-			
-			if (true)
-			{
-				
-				if (Vector3.Angle(this.transform.forward, direction) > 179)
-				{
-					direction = this.transform.TransformDirection(new Vector3(0.01f, 0, -1f));
-				}
-
-				this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
-					Quaternion.LookRotation(direction.normalized), rotateDegreesPerSecond * Time.deltaTime);
-
-				Vector3 moveDirection = this.transform.forward;
-				moveDirection.y -= gravity * Time.deltaTime;
-				
-				this.characterController.Move(moveDirection * this.movementSpeed * Time.deltaTime);
-			}
-			
-			// Manage other actions here, like jump or something
-			if (Input.GetKeyDown(this.attack))
-				this.Attack();
 		}
-		else
+		else if (Input.GetKeyUp(this.attack))
 		{
-			Debug.LogError("Null character controller");
+			
 		}
-		*/
 	}
 
 	private void OnTriggerEnter(Collider other)
---
+++ Assets/Scripts/TheDeathAnimator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheDeathAnimator : MonoBehaviour
+{
+    private Animator animator;
+    
+    private readonly int walkName = Animator.StringToHash("Walk");
+    private readonly int stopName = Animator.StringToHash("Stop");
+    private readonly int slashName = Animator.StringToHash("Slash");
+
+    private void Awake()
+    {
+        this.animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    public void Walk(float walkSpeed)
+    {
+        if (this.animator != null)
+        {
+            this.animator.SetFloat(this.walkName, walkSpeed);
+        }
+    }
+
+    public void Stop()
+    {
+        if (this.animator != null)
+        {
+            this.animator.SetTrigger(this.stopName);
+        }
+    }
+
+    public void Slash()
+    {
+        if (this.animator != null)
+        {
+            this.animator.SetTrigger(this.slashName);
+        }
+    }
+}
