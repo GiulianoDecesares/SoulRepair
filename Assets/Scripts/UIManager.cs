650b7b19ebbf753210f72114305361b4225e2c1a
Giuliano Decesares <giulianodecesares@gmail.com>
2020-02-02 18:37:40 -0300

    UI integrated

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject CreditsContent;
+    public GameObject MainMenuContent;
+
+    private void Awake()
+    {
+        EnableCredits(false);
+        EnableMain(true);
+        
+        Callbacks.OnExitCreditsClick += ExitCreditsToMenu;
+        Callbacks.OnCreditsClick += OnEnterCredits;
+    }
+
+    private void OnDestroy()
+    {
+        Callbacks.OnExitCreditsClick -= ExitCreditsToMenu;
+        Callbacks.OnCreditsClick -= OnEnterCredits;
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+        // Callbacks.NewGameClick();
+    }
+
+    public void Credits()
+    {
+        Callbacks.CreditsClick();
+    }
+
+    public void Exit()
+    {
+        Callbacks.ExitClick();
+    }
+
+    public void ExitCredits()
+    {
+        Callbacks.ExitCreditsClick();
+    }
+
+    private void ExitCreditsToMenu()
+    {
+        EnableCredits(false);
+        EnableMain(true);
+    }
+
+    private void OnEnterCredits()
+    {
+        EnableCredits(true);
+        EnableMain(false);
+    }
+
+    private void EnableCredits(bool value)
+    {
+        CreditsContent.SetActive(value);
+    }
+
+    private void EnableMain(bool value)
+    {
+        MainMenuContent.SetActive(value);
+    }
+
+    public static class Callbacks
+    {
+        public static Action OnNewGameClick;
+
+        public static void NewGameClick()
+        {
+            OnNewGameClick?.Invoke();
+        }
+
+        public static Action OnCreditsClick;
+
+        public static void CreditsClick()
+        {
+            OnCreditsClick?.Invoke();
+        }
+
+        public static Action OnExitClick;
+
+        public static void ExitClick()
+        {
+            OnExitClick?.Invoke();
+        }
+
+        public static Action OnExitCreditsClick;
+
+        public static void ExitCreditsClick()
+        {
+            OnExitCreditsClick?.Invoke();
+        }
+    }
+}
