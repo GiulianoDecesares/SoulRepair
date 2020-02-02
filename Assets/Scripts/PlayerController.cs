a29b5f5dd830392e310620e34d14887803ccd370
Giuliano Decesares <giulianodecesares@gmail.com>
2020-02-02 03:07:02 -0300

    Improved player movement. Positioned player over blockinig

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,54 +11,79 @@ using UnityEngine.PlayerLoop;
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
-	[Header("Attack")]
-	[SerializeField] private KeyCode attack;
+	[Header("Attack")] [SerializeField] private KeyCode attack;
 	[SerializeField] private float damagePerHit;
 
-	[Header("Movement")]
-	[SerializeField] private float movementSpeed;
+	[Header("Movement")] [SerializeField] private float movementSpeed;
 	[SerializeField] private float gravity;
-	
+	[SerializeField] private float rotationSpeed;
+
 	private CharacterController characterController;
-	private Vector3 moveDirection;
-	
+	Vector3 direction = Vector3.zero;
+
 	private List<WeakReference<IEnemy>> spottedEnemies;
 
 	private void Awake()
 	{
 		this.characterController = this.gameObject.GetComponent<CharacterController>();
-		this.moveDirection = Vector3.zero;
 		this.spottedEnemies = new List<WeakReference<IEnemy>>();
 	}
 
 	private void Update()
 	{
+		Vector3 rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * this.rotationSpeed * Time.deltaTime, 0);
+		Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
+
+		move = this.transform.TransformDirection(move);
+		
+		this.characterController.Move(move * this.movementSpeed);
+		this.transform.Rotate(rotation);
+		
+		/*
 		if (this.characterController != null)
 		{
+			float horizontalAxis = Input.GetAxis("Horizontal");
+			float verticalAxis = Input.GetAxis("Vertical");
+			float rotateDegreesPerSecond = 180f;
+
 			if (this.characterController.isGrounded)
 			{
-				this.moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.00f, Input.GetAxis("Vertical"));
-				this.moveDirection *= this.movementSpeed;
+				direction = new Vector3(horizontalAxis, 0.0f, verticalAxis);
+				direction = this.transform.TransformDirection(direction);
+			}
+			
+			if (true)
+			{
+				
+				if (Vector3.Angle(this.transform.forward, direction) > 179)
+				{
+					direction = this.transform.TransformDirection(new Vector3(0.01f, 0, -1f));
+				}
+
+				this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+					Quaternion.LookRotation(direction.normalized), rotateDegreesPerSecond * Time.deltaTime);
+
+				Vector3 moveDirection = this.transform.forward;
+				moveDirection.y -= gravity * Time.deltaTime;
+				
+				this.characterController.Move(moveDirection * this.movementSpeed * Time.deltaTime);
 			}
 			
 			// Manage other actions here, like jump or something
 			if (Input.GetKeyDown(this.attack))
 				this.Attack();
-			
-			// Apply gravity squared
-			this.moveDirection.y -= this.gravity * Time.deltaTime;
-			this.characterController.Move(this.moveDirection * Time.deltaTime);
 		}
 		else
 		{
 			Debug.LogError("Null character controller");
 		}
+		*/
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		IEnemy enemy = other.GetComponent<IEnemy>();
-	
+
 		if (enemy != null)
 		{
 			this.spottedEnemies.Add(new WeakReference<IEnemy>(enemy));
