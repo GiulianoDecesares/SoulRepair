64366304c0739294c0e82320022f7765b812ec01
Giuliano Decesares <giulianodecesares@gmail.com>
2020-02-10 05:56:16 -0300

    Added leaderboard flow to main menu

--- Assets/Scripts/MainMenu/WelcomeMenu.cs
+++ Assets/Scripts/MainMenu/WelcomeMenu.cs
@@ -12,15 +12,25 @@ public class WelcomeMenu : MonoBehaviour
     [SerializeField] private GameObject registerPanel;
     [SerializeField] private GameObject mainMenuButtonsContent;
     [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject leaderboardsPanel;
 
     [SerializeField] private Text identificationNameText;
+    [SerializeField] private Text leaderboardsPanelText;
 
     private string userIdentificationName;
     
+
     public void OnNewGameClick()
     {
-        this.mainMenuPanel.SetActive(false);
-        this.registerPanel.SetActive(true);
+        if (PlayFabClientAPI.IsClientLoggedIn())
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            this.mainMenuPanel.SetActive(false);
+            this.registerPanel.SetActive(true);   
+        }
     }
 
     public void OnCreditsClick()
@@ -37,6 +47,11 @@ public class WelcomeMenu : MonoBehaviour
 
     public void OnRegistrationPanelBeginClick()
     {
+        // Disable all buttons
+        Button[] registerbuttons = this.registerPanel.GetComponentsInChildren<Button>();
+        foreach (Button registerbutton in registerbuttons)
+            registerbutton.interactable = false;
+        
         // Register the player with id
         LoginWithCustomIDRequest loginRequest = new LoginWithCustomIDRequest
         {
@@ -50,14 +65,17 @@ public class WelcomeMenu : MonoBehaviour
         {
             // Catch the session ticket
             PlayFabSettings.staticPlayer.ClientSessionTicket = result.SessionTicket;
-
-            // Load playable scene
-            SceneManager.LoadScene(1);
+            
+            this.registerPanel.SetActive(false);
+            this.mainMenuPanel.SetActive(true);
         }
 
         void OnLoginFail(PlayFabError error)
         {
             Debug.Log(error.GenerateErrorReport());
+            
+            foreach (Button registerbutton in registerbuttons)
+                registerbutton.interactable = true;
         }
     }
 
@@ -70,6 +88,48 @@ public class WelcomeMenu : MonoBehaviour
     public void OnIdentificationNameFieldChanged()
     {
         this.userIdentificationName = this.identificationNameText.text;
+    }
+
+    public void OnLeaderboardsClick()
+    {
+        this.mainMenuPanel.SetActive(false);
+        this.leaderboardsPanel.SetActive(true);
+        
+        // Check if user is logged
+        if (!PlayFabClientAPI.IsClientLoggedIn())
+        {
+            return;
+        }
+        
+        // Get leaderboards request
+        GetLeaderboardRequest getLeaderboardRequest = new GetLeaderboardRequest
+        {
+            MaxResultsCount = 10,
+            StatisticName = "HighScore"
+        };
+        
+        PlayFabClientAPI.GetLeaderboard(getLeaderboardRequest, OnGetLeaderboardSuccess, OnGetLeaderboardFailed);
+
+        void OnGetLeaderboardSuccess(GetLeaderboardResult leaderboardResult)
+        {
+            this.leaderboardsPanelText.text = "";
+            
+            foreach (PlayerLeaderboardEntry entry in leaderboardResult.Leaderboard)
+            {
+                this.leaderboardsPanelText.text += $"{entry.DisplayName} : {entry.StatValue} \n";
+            }
+        }
+
+        void OnGetLeaderboardFailed(PlayFabError error)
+        {
+            Debug.Log(error.GenerateErrorReport());
+        }
+    }
+
+    public void OnLeaderboardBackClick()
+    {
+        this.leaderboardsPanel.SetActive(false);
+        this.mainMenuPanel.SetActive(true);
     }
 
     public void OnExitClick()
